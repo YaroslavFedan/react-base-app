cca2fedf421f652e3df3c0bcaa1e5812685c25f0
YaroslavFd <yaroslav.fd@gmail.com>
2019-10-21 17:47:32 +0300

    21.10.2019

--- src/modules/RegisterForm/containers/RegisterForm.js
+++ src/modules/RegisterForm/containers/RegisterForm.js
@@ -1,18 +1,4 @@
-// import React from 'react';
-// import RegisterForm from "../components/RegisterForm";
-// import RegisterSuccess from "../components/RegisterSuccess";
 
-
-// class RegisterFormContainer extends React.Component {
-
-//     render() {
-//         const { success } = this.props;
-//         return (!success ? <RegisterForm /> : <RegisterSuccess />);
-//     }
-// }
-
-
-// export default RegisterFormContainer;
 
 import RegisterForm from "../components/RegisterForm";
 
